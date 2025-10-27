632d04d079459737890c5b0308a7dcd99682bd0b
haroun <harounmoussa570@gmail.com>
2025-10-27 23:47:05 +0100

    Remove Google Analytics script from ContactPage
    Removed Google Analytics script from ContactPage.

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -11,13 +11,7 @@ const ContactPage = () => {
     <AuroraBackground>
       <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-L8Z6KSP4X3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-L8Z6KSP4X3');
-</script>
       <Box>
       {/* Header Section */}
       <SecondHeader
